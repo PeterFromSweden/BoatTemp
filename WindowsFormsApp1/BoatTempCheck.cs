a2aba7206be3fdde975886fa7bda3d23e18404c5
Peter Liedholm <peter.liedholm@securemeters.com>
2018-11-28 09:47:29 +0100

    Made use of ssh key.

--- WindowsFormsApp1/BoatTempCheck.cs
+++ WindowsFormsApp1/BoatTempCheck.cs
@@ -24,46 +24,58 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            float temp;
-            float hygroPerc;
-            List<string> sensorStrings;
-            List<string> sensorPaths = new List<string>() {
-                @"/tmp/fineoffset.temperaturehumidity.135.tmp",
-                @"/tmp/fineoffset.temperaturehumidity.199.tmp",
-                @"/tmp/mandolyn.temperaturehumidity.11.tmp"
-            };
-            //string tempStr = "-2.6°	68%     	                    	                    	2018-11-26 15:39:54";
+            try
+            {
+                float temp;
+                float hygroPerc;
+                List<string> sensorStrings;
+                List<string> sensorPaths = new List<string>() {
+                    @"/tmp/fineoffset.temperaturehumidity.135.tmp",
+                    @"/tmp/fineoffset.temperaturehumidity.199.tmp",
+                    @"/tmp/mandolyn.temperaturehumidity.11.tmp"
+                };
+                //string tempStr = "-2.6°	68%     	                    	                    	2018-11-26 15:39:54";
 
-            sensorStrings = sshAttempt(sensorPaths);
-            parseSensor(sensorStrings[0], out temp, out hygroPerc);
-            aGauge1.Value = temp;
-            txtTmpBoat.Text = temp.ToString();
-            txtHygBoat.Text = hygroPerc + "%";
+                sensorStrings = sshAttempt(sensorPaths);
+                parseSensor(sensorStrings[0], out temp, out hygroPerc);
+                aGauge1.Value = temp;
+                txtTmpBoat.Text = temp.ToString();
+                txtHygBoat.Text = hygroPerc + "%";
 
-            parseSensor(sensorStrings[1], out temp, out hygroPerc);
-            aGauge2.Value = temp;
-            txtTmpShed.Text = temp.ToString();
-            txtHy1Shed.Text = hygroPerc + "%";
+                parseSensor(sensorStrings[1], out temp, out hygroPerc);
+                aGauge2.Value = temp;
+                txtTmpShed.Text = temp.ToString();
+                txtHy1Shed.Text = hygroPerc + "%";
 
-            parseSensor(sensorStrings[2], out temp, out hygroPerc);
-            aGauge3.Value = temp;
-            txtHeatBox.Text = temp.ToString();
-            txtHy2Shed.Text = hygroPerc + "%";
-            
-            //timer1.Enabled = true;
+                parseSensor(sensorStrings[2], out temp, out hygroPerc);
+                aGauge3.Value = temp;
+                txtHeatBox.Text = temp.ToString();
+                txtHy2Shed.Text = hygroPerc + "%";
+
+                //timer1.Enabled = true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void parseSensor(string sensorString, out float temp, out float hygroPerc)
         {
+            //temp = 0.0F;
+            //hygroPerc = 0.0F;
+
             sensorString = System.Text.RegularExpressions.Regex.Replace(sensorString, @"\s+", " ");
             string[] sensorParts = sensorString.Split(null);
             string tempStr = new string((from c in sensorParts[0]
-                                  where char.IsNumber(c) || char.IsPunctuation(c)
-                              select c).ToArray());
+                                            where char.IsNumber(c) || char.IsPunctuation(c)
+                                            select c).ToArray());
             float.TryParse(tempStr, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out temp);
             string hygroStr = new string((from c in sensorParts[1]
-                                         where char.IsNumber(c)
-                                         select c).ToArray());
+                                            where char.IsNumber(c)
+                                            select c).ToArray());
             float.TryParse(hygroStr, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out hygroPerc);
         }
 
@@ -71,29 +83,22 @@ namespace WindowsFormsApp1
         {
             List<string> res = new List<string>();
 
-            try
+            var connectionInfo = new ConnectionInfo("liedholm.dynamic-dns.net",
+                                                    2210,
+                                                    "pi",
+                                                    new PrivateKeyAuthenticationMethod("pi", new PrivateKeyFile(Settings.Default.opensshppk)));
+            using (var client = new ScpClient(connectionInfo))
             {
-                var connectionInfo = new ConnectionInfo("liedholm.dynamic-dns.net",
-                                                        2210,
-                                                        "pi",
-                                                        new PrivateKeyAuthenticationMethod("pi", new PrivateKeyFile("c:\\openssh.ppk")));
-                using (var client = new ScpClient(connectionInfo))
+                client.Connect();
+                foreach (string sensorPath in sensorPaths)
                 {
-                    client.Connect();
-                    foreach (string sensorPath in sensorPaths)
+                    using (var stream = new MemoryStream())
                     {
-                        using (var stream = new MemoryStream())
-                        {
-                            client.Download(sensorPath, stream);
-                            res.Add(Encoding.UTF8.GetString(stream.ToArray()));
-                            Console.WriteLine(res.Last());
-                        }
+                        client.Download(sensorPath, stream);
+                        res.Add(Encoding.UTF8.GetString(stream.ToArray()));
+                        Console.WriteLine(res.Last());
                     }
                 }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
             }
 
             return res;
